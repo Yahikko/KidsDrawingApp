aa0863045b87d3e62f78c3ec67715452eaea0348
Yahikko <taczuhiro@mail.ru>
2023-08-06 22:16:18 +0300

    permissions; image selection functionality

--- app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.kidsdrawingapp
 
+import android.Manifest
+import android.app.AlertDialog
 import android.app.Dialog
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 
@@ -16,6 +25,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var brushSizeDialog: Dialog
     private lateinit var mImageButtonCurrentPaint: ImageButton
     private lateinit var colorsLinearLayout: LinearLayout
+
+    val openGalleryLauncher: ActivityResultLauncher<Intent> = setOpenGalleryLauncher()
+    var requestPermission: ActivityResultLauncher<Array<String>> = setRequestPermission()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,9 +42,83 @@ class MainActivity : AppCompatActivity() {
             ContextCompat.getDrawable(this, R.drawable.pallet_pressed)
         )
 
+        drawingView.setBrushSize(5.0f)
         brushSizeImgBtn.setOnClickListener { showBrushSizeChooserDialog() }
 
-        drawingView.setBrushSize(5.0f)
+        val ibGallery: ImageButton = findViewById(R.id.ib_gallery)
+        ibGallery.setOnClickListener {
+            requestStoragePermission()
+        }
+    }
+
+    private fun requestStoragePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this, Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+        ) {
+            showRationaleDialog(
+                "Kids Drawing App",
+                "Kids Drawing App needs to Access Your External Storage"
+            )
+        } else {
+            requestPermission.launch(
+                arrayOf(
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                    // TODO: Add writing external storage permission
+                )
+            )
+        }
+    }
+
+    private fun setOpenGalleryLauncher(): ActivityResultLauncher<Intent> {
+        val openGalleryLauncher: ActivityResultLauncher<Intent> =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == RESULT_OK && result.data != null) {
+                    val imageBackground: ImageView = findViewById(R.id.background_image_view)
+                    imageBackground.setImageURI(result.data?.data)
+                }
+            }
+        return openGalleryLauncher
+    }
+
+    private fun setRequestPermission(): ActivityResultLauncher<Array<String>> {
+        val requestPermission: ActivityResultLauncher<Array<String>> =
+            registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permission ->
+                permission.entries.forEach {
+                    val permissionName = it.key
+                    val isGranted = it.value
+
+                    if (isGranted) {
+                        val pickIntent = Intent(
+                            Intent.ACTION_PICK,
+                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                        )
+                        openGalleryLauncher.launch(pickIntent)
+                    } else {
+                        if (permissionName == Manifest.permission.READ_EXTERNAL_STORAGE) {
+                            Toast.makeText(
+                                this,
+                                "You just denied the permission",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+                    }
+                }
+            }
+        return requestPermission
+    }
+
+    private fun showRationaleDialog(
+        title: String,
+        message: String
+    ) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+            .setMessage(message)
+            .setPositiveButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }
+        builder.create().show()
     }
 
     fun paintClicked(view: View) {
